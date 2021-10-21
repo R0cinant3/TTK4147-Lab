def89a0b096672d1e39b34530e0401e76e0c4ac5
R0cinant3 <sindre_jf@hotmail.com>
2021-10-21 11:34:39 +0200

    test

--- ex8/TaskC/main.c
+++ ex8/TaskC/main.c
@@ -92,8 +92,7 @@ int main(){
     RT_TASK task2;
     RT_TASK task3;
 
-    rt_mutex_create(&mutex_low,"Low Mutex");
-    rt_mutex_create(&mutex_high,"High Mutex");
+    rt_mutex_create(&mutex,"Mutex");
  
 	//Creating threads
 	rt_task_create(&task1, "Task1", stack_size, TASK1_PRIO, mode);
