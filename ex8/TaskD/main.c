817b00a0eeed68c61a77b191258710fabd72ac38
R0cinant3 <sindre_jf@hotmail.com>
2021-10-21 11:44:04 +0200

    added task d

---
+++ ex8/TaskD/main.c
@@ -0,0 +1,119 @@
+//#define _GNU_SOURCE
+#include <rtdk.h>
+#include <pthread.h>
+#include <native/task.h>
+#include <native/timer.h>
+#include <native/sem.h>
+#include <native/mutex.h>
+
+#include <sys/mman.h>
+#include <unistd.h>
+
+#define TASK1_ID 1
+#define TASK2_ID 2
+#define TASK3_ID 3
+
+#define TASK1_PRIO 60
+#define TASK2_PRIO 65
+#define TASK3_PRIO 70
+
+#define TASK1_RT_TIME 0
+#define TASK2_RT_TIME 1
+#define TASK3_RT_TIME 2
+
+#define TASK1_BUSY_TIME 3
+#define TASK2_BUSY_TIME 5
+#define TASK3_BUSY_TIME 2
+
+RT_SEM sem;
+RT_MUTEX mutex_a;
+RT_MUTEX mutex_b;
+
+typedef struct TaskParameters{
+    uint8_t id;
+    uint8_t priority;
+    RTIME rt_time;
+    unsigned long busy_time;
+}TaskParameters;
+
+int set_cpu(int cpu_number){
+	cpu_set_t cpu;
+	CPU_ZERO(&cpu);
+	CPU_SET(cpu_number, &cpu);
+
+	return pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &cpu);
+}
+
+void busy_wait_us(unsigned long delay){
+    for(; delay > 0; delay--){
+        rt_timer_spin(1000);
+    }
+}
+
+void task_func(void * args){
+    set_cpu(1);
+    struct TaskParameters taskparam = *(struct TaskParameters*)args;
+    switch(taskparam.id){
+        case 1: 
+            rt_printf("Task ID: %d\tPriority: %d\r\n", taskparam.id, taskparam.priority);
+            rt_mutex_acquire(&mutex_a, TM_INFINITE);
+            busy_wait_us(3);
+            rt_mutex_acquire(&mutex_b, TM_INFINITE);
+            busy_wait_us(3);
+            rt_mutex_release(&mutex_b);
+            rt_mutex_release(&mutex_a);
+            busy_wait_us(1);
+            break;
+        case 2:
+            rt_printf("Task ID: %d\tPriority: %d\r\n", taskparam.id, taskparam.priority);
+            rt_task_sleep(1);
+            rt_mutex_acquire(&mutex_b, TM_INFINITE);
+            busy_wait_us(1);
+            rt_mutex_acquire(&mutex_a, TM_INFINITE);
+            busy_wait_us(2);
+            rt_mutex_release(&mutex_a);
+            rt_mutex_release(&mutex_b);
+            busy_wait_us(1);
+            break;
+    }
+}
+
+int main(){
+	mlockall(MCL_CURRENT|MCL_FUTURE);
+	rt_print_auto_init(1);
+	int stack_size = 0;
+	int mode = T_CPU(1);
+
+    rt_task_shadow(NULL, "Main", 65, mode);
+
+    rt_sem_create(&sem, "Semaphore", 0, S_PRIO);
+
+    RT_TASK task1;
+    RT_TASK task2;
+    RT_TASK task3;
+
+    rt_mutex_create(&mutex_low,"Low Mutex");
+    rt_mutex_create(&mutex_high,"High Mutex");
+ 
+	//Creating threads
+	rt_task_create(&task1, "Task1", stack_size, TASK1_PRIO, mode);
+	rt_task_create(&task2, "Task2", stack_size, TASK2_PRIO, mode);
+	rt_task_create(&task3, "Task3", stack_size, TASK3_PRIO, mode);
+
+	//Start threads
+	rt_task_start(&task1, task_func,(&(struct TaskParameters){TASK1_ID, TASK1_PRIO, TASK1_RT_TIME, TASK1_BUSY_TIME}));
+	rt_task_start(&task2, task_func,(&(struct TaskParameters){TASK2_ID, TASK2_PRIO, TASK2_RT_TIME, TASK2_BUSY_TIME}));
+	rt_task_start(&task3, task_func,(&(struct TaskParameters){TASK3_ID, TASK3_PRIO, TASK3_RT_TIME, TASK3_BUSY_TIME}));
+
+    rt_task_sleep(100*1000*1000);
+    rt_printf("Broadcasting...\r\n");
+    rt_sem_broadcast(&sem);
+    rt_task_sleep(100*1000*1000);
+    rt_printf("END...\r\n");
+
+    rt_sem_delete(&sem);
+
+
+    return 0;
+}
+
